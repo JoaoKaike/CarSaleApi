c3a670b3b18902d74eb0ac7772624d983ea970ad
João Kaike <kaikepastana12@gmail.com>
2026-08-27 11:37:50 -0300

    add CarController file

--- src/main/java/com/CarSale/controller/CarController.java
+++ src/main/java/com/CarSale/controller/CarController.java
@@ -1,22 +1,45 @@
 package com.CarSale.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import com.CarSale.repository.CarRepository;
+import com.CarSale.service.CarService;
+import com.CarSale.model.Car;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
 
+    private final CarService carService;
+
     @Autowired
-    private CarRepository repository;
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @PostMapping
-    @Transactional
-    public 
+    public ResponseEntity<Void> createCar(@RequestBody Car car) {
+        carService.saveCar(car);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Car> getCarById(@RequestParam Long id) {
+        return ResponseEntity.ok(carService.getCarById(id));
+        
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteCar(@RequestParam Long id) {
+        carService.deleteCar(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
