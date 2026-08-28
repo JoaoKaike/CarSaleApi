b133b29a326947289213860bba2b4136aabe9cb6
João Kaike <kaikepastana12@gmail.com>
2026-08-28 09:44:32 -0300

    fixing ClientRepository file

--- src/main/java/com/CarSale/repository/ClientRepository.java
+++ src/main/java/com/CarSale/repository/ClientRepository.java
@@ -1,5 +1,8 @@
 package com.CarSale.repository;
 
-public interface ClientRepository {
+import com.CarSale.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
