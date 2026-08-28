a9708e9d053e5fd17ac537e5f57790d30ffaef1e
João Kaike <kaikepastana12@gmail.com>
2026-08-28 09:42:52 -0300

    add ClientService

--- src/main/java/com/CarSale/service/ClientService.java
+++ src/main/java/com/CarSale/service/ClientService.java
@@ -1,6 +1,7 @@
 package com.CarSale.service;
 
 import com.CarSale.repository.ClientRepository;
+import com.CarSale.model.Client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,20 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
-    public
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public void saveClient(Client client) {
+        clientRepository.saveAndFlush(client);
+    }
+
+    public void deleteClient(Long id) {
+        clientRepository.deleteById(id);
+    }
+
+    public Client getClientById(Long id) {
+        return clientRepository.findById(id).orElse(null);
+    }
 
 }
